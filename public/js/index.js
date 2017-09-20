449806f9cc4bbd846582f0942916716a5cf81ed6
Yuriy Cat <very.good.kot@gmail.com>
2017-09-20 21:35:34 +0300

    clean js

--- public/js/index.js
+++ public/js/index.js
@@ -9,20 +9,44 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
-  console.log('New message', message);
-  var li = $('<li></li>');
-  li.text(`${message.from}: ${message.text}`);
+  // console.log('New message', message);
+  // var li = $('<li></li>');
+  // li.text(`${message.from}: ${message.text}`);
+  // $('#messages').append(li);
 
-  $('#messages').append(li);
+
+  var node = document.createElement("LI");
+  var textnode = document.createTextNode(message.from + ': ' + message.text);
+  node.appendChild(textnode);                              // Append the text to <li>
+  document.getElementById("messages").appendChild(node);
 });
 
-$('#message-form').on('submit', function (e) {
+document.getElementById("message-form").onsubmit = function(e){
+
   e.preventDefault();
+
+  console.log(document.getElementsByTagName("input")[0].value);
 
   socket.emit('createMessage', {
     from: 'User',
-    text: $('[name=message]').val()
+    text: document.getElementsByTagName("input")[0].value
   }, function () {
   });
-  $('[name=message]').val('');
-});
+
+  document.getElementsByTagName("input")[0].value = '';
+
+};
+
+
+
+
+// $('#message-form').on('submit', function (e) {
+//   e.preventDefault();
+//
+//   socket.emit('createMessage', {
+//     from: 'User',
+//     text: $('[name=message]').val()
+//   }, function () {
+//   });
+//   $('[name=message]').val('');
+// });
